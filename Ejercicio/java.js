f70325782696ac41e95b53ef4f32535144e4548e
claugc92 <claudio.gutierrez92@hotmail.com>
2021-09-24 12:13:12 -0300

    cambios

--- Ejercicio/java.js
+++ Ejercicio/java.js
@@ -1,1 +1,29 @@
-let numero = prompt ("Introduce tu edad")
+
+    const section = document.getElementsByClassName("section")
+    const newElement = document.createElement ("ol")
+    const input = document.getElementById("input")
+    const button = document.getElementsByClassName("addbuton")
+    const lista = document.getElementById("li")
+
+    const agregarText = function(){
+        let textNuevo = inputElement.value;
+        let texto = document.createElement("section")
+        texto.innerText = textNuevo;
+        input.value = "";
+        lista.appendChild(texto)
+        let btn = document.createElement("addbuton");
+        btn.innerHTML = "Sugerencias";
+        }
+
+        // if(input.value === "") return
+        // const newElement = document.createElement("p");
+        // nuevoText.textContent = input.value;
+    
+        // input.value="";
+
+        // section.appendChild(newElement);
+
+
+    
+addButton.innerHTML = "texto"
+let numero = prompt("Introduce tu edad")
